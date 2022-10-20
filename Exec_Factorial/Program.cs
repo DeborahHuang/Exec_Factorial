8dfb44efcd22f90516372cdb7151d8e0de8394ce
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-20 22:34:12 +0800

    加入專案檔案。

---
+++ Exec_Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//求1~10的階乘
+			int result = 1;
+			for (int i = 1; i <= 10; i++)
+			{
+				result *= i;
+			}
+			Console.WriteLine(result);
+		}
+	}
+}
